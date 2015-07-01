fbeda39b736c0a274f3ccbec33f167cb53f462b5
Marek Otahal <markotahal@gmail.com>
2015-07-01 21:09:10 +0200

    use Category encoder

--- short_sequences_prediction/model_params.py
+++ short_sequences_prediction/model_params.py
@@ -73,12 +73,13 @@ MODEL_PARAMS = {
 #                },
                 'number': {
                        'fieldname': u'number',
-                       'resolution': 0.05, # to have some similarity between adjacent numbers
+#                       'resolution': 0.05, # to have some similarity between adjacent numbers
                        'name': u'number',
-                       'type': 'ScalarEncoder',
+                       'type': 'CategoryEncoder',
                        'w': 31, # currently: {0..100},
-                       'minval': 0,
-                       'maxval': 10,  # based on generated data, eg for '+' and maxArg=5 this should be 10
+                       'categoryList': ["0","1","2","3","4","5","6","7","8","9","10"],
+#                       'minval': 0,
+#                       'maxval': 10,  # based on generated data, eg for '+' and maxArg=5 this should be 10
                 }
             },
 
